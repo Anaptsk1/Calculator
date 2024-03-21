373cdc62c2b015ba00f5d7b686bbb774ebc3b6c8
Ana Ptskialadze <anukiptskialadze@gmai.com>
2024-03-21 23:16:53 +0100

    1st version of Calculator.

---
+++ Calculator/Controller/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Can't convert displayLabel text to a double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    private var calculator = CalculatorLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
+}
+
